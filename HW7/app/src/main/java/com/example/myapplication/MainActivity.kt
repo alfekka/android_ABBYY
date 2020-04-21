b0683aa9f7c35169c89a3618fcb8281031f34deb
alfekka <rvanova.lyu@phystech.edu>
2020-04-21 17:10:03 +0300

    hw7

---
+++ HW7/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.title = getString(R.string.app_title)
+        val cardView = findViewById<View>(R.id.note_list_item)
+        cardView.setOnClickListener {
+            val intent = Intent(Partical@this, ParticalActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
